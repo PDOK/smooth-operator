f82cffc9e555ec249b578ea3a1e66c8298491128
Jelle Dijkstra <jelle.dijkstra@kadaster.nl>
2025-04-28 11:46:44 +0200

    Get configmap names

---
+++ pkg/k8s/clientset.go
@@ -0,0 +1,53 @@
+package k8s
+
+import (
+	"context"
+	"errors"
+	v1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"strings"
+)
+
+func GetConfigMap(k8sClient client.Client, namespace, prefix string, labels map[string]string) (*v1.ConfigMap, error) {
+	if !strings.HasSuffix(prefix, "-") {
+		prefix += "-"
+	}
+
+	configMapList := &v1.ConfigMapList{}
+	err := k8sClient.List(context.TODO(), configMapList, client.InNamespace(namespace), client.MatchingLabels(labels))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, configMap := range configMapList.Items {
+		// If the name contains "-" after the prefix, it means that the prefix is longer
+		// Example 'blobs' and 'blobs-premium'
+		if strings.HasPrefix(configMap.Name, prefix) && !strings.Contains(configMap.Name[len(prefix):], "-") {
+			return &configMap, nil
+		}
+	}
+
+	return nil, errors.New("no configmap found with prefix " + prefix)
+}
+
+func GetSecret(k8sClient client.Client, namespace, prefix string, labels map[string]string) (*v1.Secret, error) {
+	if !strings.HasSuffix(prefix, "-") {
+		prefix += "-"
+	}
+
+	secretList := &v1.SecretList{}
+	err := k8sClient.List(context.TODO(), secretList, client.InNamespace(namespace), client.MatchingLabels(labels))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, secret := range secretList.Items {
+		// If the name contains "-" after the prefix, it means that the prefix is longer
+		// Example 'blobs' and 'blobs-premium'
+		if strings.HasPrefix(secret.Name, prefix) && !strings.Contains(secret.Name[len(prefix):], "-") {
+			return &secret, nil
+		}
+	}
+
+	return nil, errors.New("no secret found with prefix " + prefix)
+}
---
+++ pkg/k8s/diff.go
@@ -0,0 +1,26 @@
+package k8s
+
+import (
+	"context"
+	"github.com/google/go-cmp/cmp"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func ShowDiff(ctx context.Context, k8sClient client.Client, obj client.Object, f controllerutil.MutateFn) {
+	lgr := log.FromContext(ctx)
+
+	err := k8sClient.Get(ctx, client.ObjectKeyFromObject(obj), obj)
+	if err != nil {
+		// Object not found, not able to show diff
+		return
+	}
+
+	current := obj.DeepCopyObject().(client.Object)
+	_ = f()
+
+	if diff := cmp.Diff(current, obj); diff != "" {
+		lgr.Info(diff)
+	}
+}

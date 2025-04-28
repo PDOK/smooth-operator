a9dd11cbb0db63806c9dd7e6eb0a6b1e5e94f4bb
Wouter Remijn <wouter.remijn@kadaster.nl>
2025-04-28 13:50:31 +0200

    fix polygon

--- model/bbox.go
+++ model/bbox.go
@@ -31,11 +31,11 @@ func (b BBox) ToExtent() string {
 
 func (b BBox) ToPolygon() string {
 	var sb strings.Builder
-	sb.WriteString(b.MinX + " " + b.MinY + " ") // lower left
-	sb.WriteString(b.MinX + " " + b.MaxY + " ") // upper left
-	sb.WriteString(b.MaxX + " " + b.MaxY + " ") // upper right
-	sb.WriteString(b.MaxX + " " + b.MinY + " ") // lower right
-	sb.WriteString(b.MinX + " " + b.MinY)       // lower left, final vertice is equal to the first one
+	sb.WriteString(b.MinY + " " + b.MinX + " ") // lower left
+	sb.WriteString(b.MinY + " " + b.MaxX + " ") // upper left
+	sb.WriteString(b.MaxY + " " + b.MaxX + " ") // upper right
+	sb.WriteString(b.MaxY + " " + b.MinX + " ") // lower right
+	sb.WriteString(b.MinY + " " + b.MinX)       // lower left, final vertice is equal to the first one
 	return sb.String()
 }
 

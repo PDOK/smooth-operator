a44b0d7fd16f55cb01e76680ca02927a140f5fb3
Jelle Dijkstra <jelle.dijkstra@kadaster.nl>
2025-05-28 09:48:38 +0200

    Added explanation on maxItems

--- model/ingress_route_urls.go
+++ model/ingress_route_urls.go
@@ -2,6 +2,7 @@ package model
 
 // IngressRouteURLs
 // +kubebuilder:validation:MinItems:=1
+// Without maxItems x-kubernetes-validation complains about an exceeded computation budget as the list can grow infinitely in theory
 // +kubebuilder:validation:MaxItems:=30
 type IngressRouteURLs []IngressRouteURL
 

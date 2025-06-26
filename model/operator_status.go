0fc9d1c987feb771df6b7e40eefbf6d291ee4987
Jelle Dijkstra <jelle.dijkstra@kadaster.nl>
2025-06-26 11:18:18 +0200

    Status subresource

--- model/operator_status.go
+++ model/operator_status.go
@@ -5,11 +5,21 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
+type ReplicaSetStatus struct {
+	Generation  int32 `json:"generation"`
+	Total       int32 `json:"total"`
+	Ready       int32 `json:"ready"`
+	Available   int32 `json:"available"`
+	Unavailable int32 `json:"unavailable"`
+}
+
+type PodSummary []ReplicaSetStatus
+
 // OperatorStatus defines the observed state of an Atom/WFS/WMS/....
 type OperatorStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-
+	PodSummary PodSummary `json:"podSummary,omitempty"`
 	// Each condition contains details for one aspect of the current state of this Atom.
 	// Known .status.conditions.type are: "Reconciled"
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
